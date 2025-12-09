24592481bb975d625d76721989ea4400df86ef2c
Zdeněk <znk.fltn@gmail.com>
2025-12-09 18:59:13 +0100

    Added tailwind

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,23 +1,5 @@
-import { createSignal } from "solid-js";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = createSignal(0);
-
-  return (
-    <>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)} type="button">
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">Click on the Vite and Solid logos to learn more</p>
-    </>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
